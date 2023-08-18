8a7c627ae1b89948267a717f16bc7cfbe5204520
Bruno N Fernandes <bruno@mercadopleno.com.br>
2023-08-18 20:17:13 -0300

    refatoração dos boxes

--- src/FeiraFacil/ConsoleApp/Program.cs
+++ src/FeiraFacil/ConsoleApp/Program.cs
@@ -6,31 +6,42 @@ namespace ConsoleApp
     {
         private static readonly Sistema _sistema = new Sistema();
         private static readonly ITabela _tabelaProduto = CreateTabelaProduto();
+        private static readonly ITabela _tabelaCliente = CreateTabelaCliente();
 
 
         public static void Main(string[] args)
         {
+            _sistema.Seed();
             var app = new App(false);
             app.AddMenu("0", "Inicializar Sistema", _sistema.Seed);
-            app.AddMenu("1", "Listar Produtos", Produtos);
-            app.AddMenu("1", "Nova Venda", _sistema.Seed);
+            app.AddMenu("1", "Listar Produtos", () => _tabelaProduto.Draw(_sistema.Produtos));
+            app.AddMenu("2", "Listar Clientes", () => _tabelaCliente.Draw(_sistema.Clientes));
+            app.AddMenu("3", "Nova Venda", () => Vender());
             app.AddMenu("X", "Sair do Sistema", app.Sair);
             app.Run();
         }
 
-        private static void Produtos()
+        private static void Vender()
         {
             _tabelaProduto.Draw(_sistema.Produtos);
         }
 
         private static ITabela CreateTabelaProduto()
         {
-            var tabela = new Tabela<Produto>();
+            var tabela = new Tabela<Produto>("Cadastro de Produtos");
             tabela.Add("Codigo", 6, x => $"{x.Codigo}");
             tabela.Add("Nome", -30, x => $"{x.Nome}");
             tabela.Add("Estoque", 7, x => $"{x.Estoque:0.000}");
             tabela.Add("Custo", 10, x => $"{x.Custo:0.00}");
             tabela.Add("Preco", 10, x => $"{x.Preco:0.00}");
+            return tabela;
+        }
+
+        private static ITabela CreateTabelaCliente()
+        {
+            var tabela = new Tabela<Cliente>("Cadastro de Clientes");
+            tabela.Add("Codigo", 6, x => $"{x.Codigo}");
+            tabela.Add("Nome", -30, x => $"{x.Nome}");
             return tabela;
         }
     }
--- src/FeiraFacil/ConsoleApp/Tabela.cs
+++ src/FeiraFacil/ConsoleApp/Tabela.cs
@@ -10,14 +10,19 @@ namespace ConsoleApp
         void Draw<TItem2>(IEnumerable<TItem2> items);
     }
 
+    /// <summary>
+    /// https://www.w3schools.com/charsets/ref_utf_box.asp
+    /// </summary>
+    /// <typeparam name="TItem"></typeparam>
     public class Tabela<TItem> : ITabela
     {
-        public const string Top = "┌┬┐";
-        public const string Mid = "├┼┤";
-        public const string Det = "│││";
-        public const string Fot = "└┴┘";
-
         private readonly List<Field> _fields = new List<Field>();
+        private readonly string _title;
+
+        public Tabela(string title)
+        {
+            _title = title;
+        }
 
         public void Add(string title, int length, Func<TItem, string> selector)
             => _fields.Add(new Field(title, length, selector));
@@ -26,35 +31,82 @@ namespace ConsoleApp
 
         public void Draw(IEnumerable<TItem> items)
         {
-            Console.WriteLine($"┌─{string.Join("─┬─", _fields.Select(x => x.Border))}─┐");
-            Console.WriteLine($"│ {string.Join(" │ ", _fields.Select(x => x.Column))} │");
-            Console.WriteLine($"├─{string.Join("─┼─", _fields.Select(x => x.Border))}─┤");
+            var paddingRight = _fields.Count * 3 + _fields.Sum(x => Math.Abs(x.Length)) - 3;
+            var paddingLeft = (paddingRight + _title.Length) / 2;
+
+            Console.WriteLine($"╔═{string.Join("═══", _fields.Select(x => x.Border2))}═╗");
+            Console.WriteLine($"║ {_title.PadLeft(paddingLeft).PadRight(paddingRight)} ║");
+            Console.WriteLine($"╠═{string.Join("═╦═", _fields.Select(x => x.Border2))}═╣");
+            Console.WriteLine($"║ {string.Join(" ║ ", _fields.Select(x => x.Column1))} ║");
+            Console.WriteLine($"╠═{string.Join("═╬═", _fields.Select(x => x.Border2))}═╣");
+            Loop(items, it => $"║ {string.Join(" ║ ", _fields.Select(x => x.Get(it)))} ║");
+            Console.WriteLine($"╚═{string.Join("═╩═", _fields.Select(x => x.Border2))}═╝");
+        }
+
+        private void Loop(IEnumerable<TItem> items, Func<TItem, string> draw)
+        {
             foreach (var item in items)
-                Console.WriteLine($"│ {string.Join(" │ ", _fields.Select(x => x.Get(item)))} │");
-            Console.WriteLine($"└─{string.Join("─┴─", _fields.Select(x => x.Border))}─┘");
+                Console.WriteLine(draw.Invoke(item));
         }
 
         public class Field
         {
-            private readonly int _length;
             private readonly Func<TItem, string> _selector;
+            public readonly int Length;
 
-            public readonly string Border;
-            public readonly string Column;
+            public readonly string Border1;
+            public readonly string Border2;
+            public readonly string Column1;
 
             public Field(string title, int length, Func<TItem, string> selector)
             {
-                _length = length;
-                Column = _length > 0 ? title.PadLeft(_length) : title.PadRight(-_length);
-                Border = new string('─', Math.Abs(_length));
+                Length = length;
+                Column1 = Length > 0 ? title.PadLeft(Length) : title.PadRight(-Length);
+                Border1 = new string('─', Math.Abs(Length));
+                Border2 = new string('═', Math.Abs(Length));
                 _selector = selector;
             }
 
             public string Get(TItem item)
             {
                 var value = _selector?.Invoke(item);
-                return _length > 0 ? value?.PadLeft(_length) : value?.PadRight(-_length);
+                return Length > 0 ? value?.PadLeft(Length) : value?.PadRight(-Length);
             }
+        }
+
+
+        public static class Box
+        {
+            public const string box1 = @"
+┌───┐
+│   │
+├─┬─┤
+│ │ │
+├─┼─┤
+│ │ │
+└─┴─┘
+";
+
+            public const string box2 = @"
+╔═══╗
+║   ║
+╠═╦═╣
+║ ║ ║
+╠═╬═╣
+║ ║ ║
+╚═╩═╝
+";
+
+            public const string box3 = @"
+╔═══╗
+║   ║
+╠═╤═╣
+║ │ ║
+╟─┼─╢
+║ │ ║
+╚═╧═╝
+";
+
         }
     }
 }
