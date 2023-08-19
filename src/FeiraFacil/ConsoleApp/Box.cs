73b7c89ba4115f3445a37f7bea40f2bc079d8ba1
Bruno N Fernandes <bruno@mercadopleno.com.br>
2023-08-19 01:10:54 -0300

    organize classes

--- src/FeiraFacil/ConsoleApp/Box.cs
+++ src/FeiraFacil/ConsoleApp/Box.cs
@@ -6,38 +6,6 @@ using System.Linq;
 
 namespace ConsoleApp
 {
-    /// <summary>
-    /// https://www.w3schools.com/charsets/ref_utf_box.asp
-    /// </summary>
-    public interface IBox
-    {
-        void Draw<TType>(IEnumerable<TType> items);
-
-        public static readonly string[,] LineBorder1 = new string[,]
-        {
-            { "──", "───", "──" },
-            { "┌─", "───", "─┐" },
-            { "│ ", "   ", " │" },
-            { "├─", "─┬─", "─┤" },
-            { "│ ", " │ ", " │" },
-            { "├─", "─┼─", "─┤" },
-            { "│ ", " │ ", " │" },
-            { "└─", "─┴─", "─┘" },
-        };
-
-        public static readonly string[,] LineBorder2 = new string[,]
-        {
-            { "══", "═══", "══" },
-            { "╔═", "═══", "═╗" },
-            { "║ ", "   ", " ║" },
-            { "╠═", "═╦═", "═╣" },
-            { "║ ", " ║ ", " ║" },
-            { "╠═", "═╬═", "═╣" },
-            { "║ ", " ║ ", " ║" },
-            { "╚═", "═╩═", "═╝" },
-        };
-    }
-
     public class Box<TItem> : IBox
     {
         private readonly ConsoleColor[] _colors;
---
+++ src/FeiraFacil/ConsoleApp/IBox.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleApp
+{
+    /// <summary>
+    /// https://www.w3schools.com/charsets/ref_utf_box.asp
+    /// </summary>
+    public interface IBox
+    {
+        void Draw<TType>(IEnumerable<TType> items);
+
+        public static readonly string[,] LineBorder1 = new string[,]
+        {
+            { "──", "───", "──" },
+            { "┌─", "───", "─┐" },
+            { "│ ", "   ", " │" },
+            { "├─", "─┬─", "─┤" },
+            { "│ ", " │ ", " │" },
+            { "├─", "─┼─", "─┤" },
+            { "│ ", " │ ", " │" },
+            { "└─", "─┴─", "─┘" },
+        };
+
+        public static readonly string[,] LineBorder2 = new string[,]
+        {
+            { "══", "═══", "══" },
+            { "╔═", "═══", "═╗" },
+            { "║ ", "   ", " ║" },
+            { "╠═", "═╦═", "═╣" },
+            { "║ ", " ║ ", " ║" },
+            { "╠═", "═╬═", "═╣" },
+            { "║ ", " ║ ", " ║" },
+            { "╚═", "═╩═", "═╝" },
+        };
+    }
+}
